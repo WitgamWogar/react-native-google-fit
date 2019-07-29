68546f46b0e135a4d27c5e0b232dceddfad8d217
Tyler Otto <tyler.otto@vuria.com>
2019-07-29 13:55:09 -0700

    Changes

--- android/src/main/java/com/reactnative/googlefit/GoogleFitManager.java
+++ android/src/main/java/com/reactnative/googlefit/GoogleFitManager.java
@@ -14,8 +14,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import android.util.Log;
 
 import com.facebook.react.bridge.ActivityEventListener;
